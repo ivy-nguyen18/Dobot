c5f98256f8ebf9b94de76bee7bb337578a8a04cc
Ivy Nguyen <nguyen.ivy1802@knights.ucf.edu>
2021-10-22 13:36:03 -0400

    Joint movement
    Add change movement in SetMove function.

--- test_works_autodisconnect_and_write_with_Trigger.py
+++ test_works_autodisconnect_and_write_with_Trigger.py
@@ -25,7 +25,7 @@ def signal():
 
 #getting input signal
 def button():
-	dType.SetIOMultiplexingExtEx(api, 7, 3, 1);
+	dType.SetIOMultiplexingExtEx(api, 10, 3, 1);
 
 #sending output signal
 def testSignal():
@@ -44,11 +44,11 @@ def off():
 
 def getPoints():
 	#change file name per run (otherwise, it will be overwritten)
-	file = open("filename.txt", "w")
+	file = open("filename1.txt", "w")
 	print(os.path.dirname(os.path.abspath("filename.txt")))
 	headers = "time(ms)\ttrigger\tinput\tx\t\ty\t\t\tz\n\n"
 	file.write(headers)
-
+	
 	while(flag == 0):
 		#get time
 		utc_dt = datetime.now(timezone.utc) # UTC time
@@ -56,7 +56,7 @@ def getPoints():
 
 		#format data
 		txt = "{:<10}"
-		data = timeMS + "\t" + str(dType.GetIODOExt(api,6)) + "\t" + str(dType.GetIODIExt(api,7)[0]) + "\t" +  txt.format(dType.GetPoseEx(api,1)) + "\t" + txt.format(dType.GetPoseEx(api,2)) + "\t" + txt.format(dType.GetPoseEx(api,3)) + "\n"
+		data = timeMS + "\t" + str(dType.GetIODOExt(api,6)) + "\t" + str(dType.GetIODIExt(api,10)[0]) + "\t" +  txt.format(dType.GetPoseEx(api,1)) + "\t" + txt.format(dType.GetPoseEx(api,2)) + "\t" + txt.format(dType.GetPoseEx(api,3)) + "\n"
 
 		#write to file
 		file.write(data)
@@ -68,28 +68,31 @@ def getPoints():
 def movementFunc():
 	def setPenPosition():
 		dType.SetPTPCmdEx(api, 0, 226.3027, 0, (-38.5), 0, 1)
+		dType.SetARCParams(api,100,100,100,100)
 
-	def move():
+	def move(x, y, z, r, changeX, changeY, changeZ, changeR):
 		global speed2
-		for count in range(1):
+		currentPosition = dType.GetPose(api)
+		print(currentPosition[0])
+		for count in range(4):
 			dType.SetArmSpeedRatioEx(api, 1, 100, 1)
-			dType.SetPTPCmdEx(api, 7, 0,  5,  0, 0, 1)
-			dType.SetPTPCmdEx(api, 7, 0,  (-5),  0, 0, 1)
-			dType.dSleep(2000)
+			if (y != 0) and (changeY != 0) :
+				dType.SetPTPCmdEx(api, 4, currentPosition[4] + y,  currentPosition[5],  currentPosition[6], currentPosition[7], 1)
+				dType.SetPTPCmdEx(api, 4, currentPosition[4] + changeY,  currentPosition[5],  currentPosition[6], currentPosition[7], 1)
+			else:
+				dType.SetPTPCmdEx(api, 7, x,  y,  z, r, 1)
+				dType.SetPTPCmdEx(api, 7, changeX, changeY,  changeZ, changeR, 1)
+			dType.dSleep(1000)
 			speed2 = dType.GetArmSpeedRatio(api, 1)[0]
 		off()
-	def move2():
-		global speed1
-		for count2 in range(1):
-			dType.SetArmSpeedRatioEx(api, 1, 100, 1)
-			dType.SetPTPCmdEx(api, 7, 5,  0,  0, 0, 1)
-			dType.SetPTPCmdEx(api, 7, (-5),  0,  0, 0, 1)
-			dType.dSleep(2000)
-			speed1 = dType.GetArmSpeedRatio(api, 1)[0]
 
+	def setMove():
+		#move( x, y, z, r, change in x, change in Y, change in Z)
+		move(20,0,0,0, -20,0,0,0)
+		move(0,20,0,0, 0,-20,0,0)
+	
 	setPenPosition()
-	move()
-	move2()
+	setMove()
 
 	#End Signal -> turns LED off
 	off()
