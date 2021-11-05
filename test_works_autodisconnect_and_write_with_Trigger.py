a390c0bb2d599c647cfabbe150b3763f9049f1c4
Jinfeng Li <45974788+BRaINmelon@users.noreply.github.com>
2021-11-05 09:48:45 -0400

    Update test_works_autodisconnect_and_write_with_Trigger.py
    include both Y-axis linear movement and J1 rotation

--- test_works_autodisconnect_and_write_with_Trigger.py
+++ test_works_autodisconnect_and_write_with_Trigger.py
@@ -67,24 +67,24 @@ def getPoints():
 
 def movementFunc():
 	def setPenPosition():
-		#dType.SetPTPCmdEx(api, 2, 270, 0, (-80.5), 0, 1)
-		dType.SetPTPCmdEx(api, 2, 226.3025, 0, (-38.5), 0, 1)
+		dType.SetPTPCmdEx(api, 2, 270, 0, (-80.5), 0, 1)
+		#dType.SetPTPCmdEx(api, 2, 226.3025, 0, (-38.5), 0, 1)
 		dType.SetARCParams(api,100,100,100,100)
 
-	def move(x, y, z, r, changeX, changeY, changeZ, changeR):
+	def move(x, y, z, r, changeX, changeY, changeZ, changeR, J1, changeJ1):    # xyzr for Cartesian coordinate system, J for joint coordinate system
 		global speed2
 		currentPosition = dType.GetPose(api)
-		for count in range(4):
+		for count in range(4):                                 
 			dType.SetArmSpeedRatioEx(api, 1, 100, 1)
-			if (y != 0) and (changeY != 0) :
-				dType.SetPTPCmdEx(api, 4, currentPosition[4] + y,  currentPosition[5],  currentPosition[6], currentPosition[7], 1)
-				dType.SetPTPCmdEx(api, 4, currentPosition[4] + changeY,  currentPosition[5],  currentPosition[6], currentPosition[7], 1)
+			if (J1 != 0):                                    
+				dType.SetPTPCmdEx(api, 4, currentPosition[4] + J1,  currentPosition[5],  currentPosition[6], currentPosition[7], 1)        # mode 4, joint mode in joint coordinate system    
+				dType.SetPTPCmdEx(api, 4, currentPosition[4] + changeJ1,  currentPosition[5],  currentPosition[6], currentPosition[7], 1)
 			else:
-				dType.SetPTPCmdEx(api, 7, x,  y,  z, r, 1)
+				dType.SetPTPCmdEx(api, 7, x,  y,  z, r, 1)                                                                                 # mode 7, linear mode in Cartesian coordinate system
 				dType.SetPTPCmdEx(api, 7, changeX, changeY,  changeZ, changeR, 1)
 			dType.dSleep(1000)
 			speed2 = dType.GetArmSpeedRatio(api, 1)[0]
-	def wait(waitTime):
+	def wait(waitTime):                                      
 		global speed2
 		for count2 in range(1):
 			dType.SetArmSpeedRatioEx(api, 1, 100, 1)
@@ -94,25 +94,29 @@ def movementFunc():
 			speed1 = dType.GetArmSpeedRatio(api, 1)[0]
 		off()
 
-	def setMove():
-		#move( x, y, z, r, change in x, change in Y, change in Z)
+	def setMove():                                                        
+		#move( x, y, z, r, change in x, change in Y, change in Z, J1, change in J1)
 	
-		move(20,0,0,0, -20,0,0,0) # move on X axis
-		move(0,20,0,0, 0,-20,0,0) # rotate on Y axis
+		move(20,0,0,0, -20,0,0,0, 0,0) # move on X axis
+		move(0,0,0,0, 0,0,0,0, 20,-20) # rotate Joint1, around Z axis
 		setPenPosition() #Reset pen position 
 
 		#Test movement starts here
-		move(1,0,0,0, -1,0,0,0) #move on X by 1
-		move(3,0,0,0, -3,0,0,0) #move on X by 3
-		move(5,0,0,0, -5,0,0,0) #move on X by 5
+		move(1,0,0,0, -1,0,0,0, 0,0) #move on X by 1
+		move(3,0,0,0, -3,0,0,0, 0,0) #move on X by 3
+		move(5,0,0,0, -5,0,0,0, 0,0) #move on X by 5
 		
-		#move(0,1,0,0, 0,-1,0,0) #move on Y by 1
-		#move(0,3,0,0, 0,-3,0,0) #move on Y by 3
-		#move(0,5,0,0, 0,-5,0,0) #move on Y by 5
+		move(0,1,0,0, 0,-1,0,0, 0,0) #move on Y by 1
+		move(0,3,0,0, 0,-3,0,0, 0,0) #move on Y by 3
+		move(0,5,0,0, 0,-5,0,0, 0,0) #move on Y by 5
 
-		move(0,0,1,0, 0,0,-1,0) #move on Z by 1
-		move(0,0,3,0, 0,0,-3,0) #move on Z by 3
-		move(0,0,5,0, 0,0,-5,0) #move on Z by 5
+		move(0,0,1,0, 0,0,-1,0, 0,0) #move on Z by 1
+		move(0,0,3,0, 0,0,-3,0, 0,0) #move on Z by 3
+		move(0,0,5,0, 0,0,-5,0, 0,0) #move on Z by 5
+
+		move(0,0,0,0, 0,0,0,0, 1,-1) # rotate Joint1 by 1°
+		move(0,0,0,0, 0,0,0,0, 3,-3) # rotate Joint1 by 3°
+		move(0,0,0,0, 0,0,0,0, 5,-5) # rotate Joint1 by 5°
 	
 	setPenPosition()
 	wait(25000)
